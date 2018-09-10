c039a0abd5d8cd06e17816c327558b4ac9ebf2f1
Paul Suarez <paulsblogging.com@gmail.com>
2018-09-10 15:28:56 -0700

    got splash screen and button to api screen with back button back to home

--- app/src/main/java/com/example/paulsuarez/avatellandroid/MainActivity.java
+++ app/src/main/java/com/example/paulsuarez/avatellandroid/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.paulsuarez.avatellandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -13,36 +17,21 @@ import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton enter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView mTextView = (TextView) findViewById(R.id.text);
+        enter = findViewById(R.id.enter);
 
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "http://www.avatell.net/";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        mTextView.setText("Response is: "+ response.substring(0,100));
-                    }
-                }, new Response.ErrorListener() {
+        enter.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onErrorResponse(VolleyError error) {
-                mTextView.setText("That didn't work! " + error.getMessage());
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, API.class);
+                startActivity(intent);
             }
         });
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
-
     }
-
-
-
 }
