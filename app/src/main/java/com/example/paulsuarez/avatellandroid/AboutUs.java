0ce8bd6d6aa3064f5082606146508ba535c40cec
Amy Cohen <anbcohen@gmail.com>
2018-09-14 10:56:04 -0700

    made github and linkedin clickable and added panos's bio

--- app/src/main/java/com/example/paulsuarez/avatellandroid/AboutUs.java
+++ app/src/main/java/com/example/paulsuarez/avatellandroid/AboutUs.java
@@ -1,16 +1,25 @@
 package com.example.paulsuarez.avatellandroid;
 
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AboutUs extends AppCompatActivity {
 
 
     private ImageButton home;
+    private ImageView panosGitHub;
+    private ImageView panosLinkedIn;
+    private ImageView paulGitHub;
+    private ImageView paulLinkedIn;
+    private ImageView amyGitHub;
+    private ImageView amyLinkedIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +28,6 @@ public class AboutUs extends AppCompatActivity {
 
 
         home = findViewById(R.id.home);
-
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,7 +37,61 @@ public class AboutUs extends AppCompatActivity {
             }
         });
 
+        //Panos Navigation
+        panosGitHub = findViewById(R.id.panosGitHub);
+        panosGitHub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToUrl("https://github.com/spinaltaper");
+            }
+        });
+        panosLinkedIn = findViewById(R.id.panosLinkedIn);
+        panosLinkedIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToUrl("https://www.linkedin.com/in/panos-kumasaka/");
+            }
+        });
+
+        //Paul Navigation
+        paulGitHub = findViewById(R.id.paulGitHub);
+        paulGitHub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToUrl("https://github.com/PaulSuarez1");
+            }
+        });
+        paulLinkedIn = findViewById(R.id.paulLinkedIn);
+        paulLinkedIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToUrl("https://www.linkedin.com/in/paulsuarezsales/");
+            }
+        });
+
+        //Amy Navigation
+        amyGitHub = findViewById(R.id.amyGitHub);
+        amyGitHub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToUrl("https://github.com/AmyCohen");
+            }
+        });
+        amyLinkedIn = findViewById(R.id.amyLinkedIn);
+        amyLinkedIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToUrl("https://www.linkedin.com/in/amyvcohen/");
+            }
+        });
 
 
+
+    }
+
+    private void goToUrl (String url) {
+        Uri uriUrl = Uri.parse(url);
+        Intent WebView = new Intent(Intent.ACTION_VIEW, uriUrl);
+        startActivity(WebView);
     }
 }
